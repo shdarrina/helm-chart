8aea8cd5156db548eab73291fccd48730e5877e5
Darrin Allred <darrin.allred@selecthealth.org>
2023-10-28 22:28:06 -0600

    Remove helm repo files from main branch

--- publish.ps1
+++
@@ -1,6 +0,0 @@
-Remove-Item .\temp -Recurse -Force -ErrorAction Ignore 
-New-Item .\temp -ItemType Directory >$NUL
-helm package -u -d .\temp\ .\helm-chart-sources\webapi\
-helm repo index --url "https://shdarrina.github.io/helm-chart" --merge .\index.yaml temp\
-Move-Item .\temp\* . -Force
-Remove-Item .\temp -Recurse -Force -ErrorAction Ignore 
